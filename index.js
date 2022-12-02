aaafc0e335836af3ac1f12f140791132c4d68703
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-02 17:38:33 -0600

    question to add more members loops

--- index.js
+++ index.js
@@ -61,24 +61,27 @@ function createIntern() {
 
 function runInquire() {
     createManager();
-    inquirer.prompt([{
-        name: "addMember",
-        message: "Choose which member to add, or choose finish:",
-        type: "list",
-        choices: [
-            "Engineer",
-            "Intern",
-            "Finish"
-        ]
-    }
-    ]).then((response) => {
-        if(response.addMember === "Engineer") {
-            createEngineer();
-        } else if (response.addMember === "Intern") {
-            createIntern();
-        } else {
-            //finished with adding members
+    let finished = false;
+    while(!finished) {
+        inquirer.prompt([{
+            name: "addMember",
+            message: "Choose which member to add, or choose finish:",
+            type: "list",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Finish"
+            ]
         }
-    })
+        ]).then((response) => {
+            if(response.addMember === "Engineer") {
+                createEngineer();
+            } else if (response.addMember === "Intern") {
+                createIntern();
+            } else {
+                finished = true;
+            }
+        })
+    }
 }
 runInquire();
