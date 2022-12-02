8dd1bf76d4f15d4496d67e4aa099881d0ad6912d
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-02 17:14:56 -0600

    added array for manager questions

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const managerQuestions = [
+    {
+        name: "managerName",
+        message: "What is your name?",
+        type: "input"
+    },
+    {
+        name: "managerID",
+        message: "What is your ID?",
+        type: "input"
+    },
+    {
+        name: "managerEmail",
+        message: "What is your email?",
+        type: "input"
+    },
+    {
+        name: "managerOfficeNumber",
+        message: "What is your office number?",
+        type: "input"
+    }
+]
