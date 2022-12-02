7a9f58b50bdc821586a99b10b24efcaa49d101a6
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-02 17:57:50 -0600

    exported classes and imported in index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
 let arr = [];
 
 const managerQuestions = [
