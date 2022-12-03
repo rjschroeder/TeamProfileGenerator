3a8a9ebb61a9e1b3b145057074bf5151bf2da7b5
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-02 19:24:43 -0600

    initialized functions for creating each HTML section

---
+++ src/template.js
@@ -0,0 +1,15 @@
+const generateHTML = teamArray => {
+    let page = [];
+
+    const generateManagerHTML = manager => {
+        
+    }
+
+    const generateEngineerHTML = engineer => {
+
+    }
+
+    const generateInternHTML = intern => {
+
+    }
+}
