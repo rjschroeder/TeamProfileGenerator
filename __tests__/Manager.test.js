aa7555e9b6dd218077899115e29ad2451bf5110a
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-02 20:39:03 -0600

    fixed copy paste error from other tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,12 +2,12 @@ const Manager = require("../lib/Manager");
 
 test("Is able to set office number?", () => {
     let testObject = new Manager("Jack", 8667, "jack@gmail.com", 65);
-    expect(testObject.github).toBe(65);
+    expect(testObject.getOfficeNumber()).toBe(65);
 });
 
 test("Is able to get office number from getter?", () => {
     let testObject = new Manager("Jack", 8667, "jack@gmail.com", 65);
-    expect(testObject.getGithub()).toBe(65);
+    expect(testObject.getOfficeNumber()).toBe(65);
 });
 
 test("Is able to get role from getter?", () => {
