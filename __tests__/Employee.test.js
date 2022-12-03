3f67514c1ab7c68fce23e6d3a4367e408646c309
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-02 20:49:00 -0600

    added comments to unit tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,35 +1,42 @@
 const Employee = require("../lib/Employee");
 
+//tests ability to set the name for an employee object
 test("Is able to set name?", () => {
     let testObject = new Employee("Jack");
     expect(testObject.name).toBe("Jack");
 });
 
+//tests ability to set the ID for an employee object
 test("Is able to set ID?", () => {
     let testObject = new Employee("Jack", 8667);
     expect(testObject.id).toBe(8667);
 });
 
+//tests ability to set the email for an employee object
 test("Is able to set Email?", () => {
     let testObject = new Employee("Jack", 8667, "jack@gmail.com");
     expect(testObject.email).toBe("jack@gmail.com");
 });
 
+//tests ability to use the getter method getName() for an employee object
 test("Is able to get name from getter?", () => {
     let testObject = new Employee("Jack");
     expect(testObject.getName()).toBe("Jack");
 });
 
+//tests ability to use the getter method getID() for an employee object
 test("Is able to get ID from getter?", () => {
     let testObject = new Employee("Jack", 8667);
     expect(testObject.getID()).toBe(8667);
 });
 
+//tests ability to use the getter method getEmail() for an employee object
 test("Is able to get Email from getter?", () => {
     let testObject = new Employee("Jack", 8667, "jack@gmail.com");
     expect(testObject.getEmail()).toBe("jack@gmail.com");
 });
 
+//tests ability to use the getter method getRole() for an employee object
 test("Is able to get role from getter?", () => {
     let testObject = new Employee("Jack", 8667, "jack@gmail.com");
     expect(testObject.getRole()).toBe("Employee");
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,15 +1,18 @@
 const Intern = require("../lib/Intern");
 
+//tests ability to set the school for an intern object
 test("Is able to set school?", () => {
     let testObject = new Intern("Jack", 8667, "jack@gmail.com", "harvard");
     expect(testObject.getSchool()).toBe("harvard");
 });
 
+//tests ability to use the getter method getSchool() for an intern object
 test("Is able to get school from getter?", () => {
     let testObject = new Intern("Jack", 8667, "jack@gmail.com", "harvard");
     expect(testObject.getSchool()).toBe("harvard");
 });
 
+//tests ability to use the getter method getRole() for an intern object
 test("Is able to get role from getter?", () => {
     let testObject = new Intern("Jack", 8667, "jack@gmail.com", "harvard");
     expect(testObject.getRole()).toBe("Intern");
