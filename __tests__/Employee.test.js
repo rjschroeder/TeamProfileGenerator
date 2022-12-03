45da4b389b9f009bb798b4b22d53a41c076f9722
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-02 20:27:06 -0600

    unit tests for getters for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -14,3 +14,18 @@ test("Is able to set Email?", () => {
     let testObject = new Employee("Jack", 8667, "jack@gmail.com");
     expect(testObject.email).toBe("jack@gmail.com");
 });
+
+test("Is able to get name from getter?", () => {
+    let testObject = new Employee("Jack");
+    expect(testObject.getName()).toBe("Jack");
+});
+
+test("Is able to get ID from getter?", () => {
+    let testObject = new Employee("Jack", 8667);
+    expect(testObject.getID()).toBe(8667);
+});
+
+test("Is able to get Email from getter?", () => {
+    let testObject = new Employee("Jack", 8667, "jack@gmail.com");
+    expect(testObject.getEmail()).toBe("jack@gmail.com");
+});
