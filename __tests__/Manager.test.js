53434602249bb8d06353ca21d9bc50ee47e7417e
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-02 20:36:10 -0600

    manager unit test for setting, getter, and role

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,16 @@
+const Manager = require("../lib/Manager");
+
+test("Is able to set office number?", () => {
+    let testObject = new Manager("Jack", 8667, "jack@gmail.com", 65);
+    expect(testObject.github).toBe(65);
+});
+
+test("Is able to get office number from getter?", () => {
+    let testObject = new Manager("Jack", 8667, "jack@gmail.com", 65);
+    expect(testObject.getGithub()).toBe(65);
+});
+
+test("Is able to get role from getter?", () => {
+    let testObject = new Manager("Jack", 8667, "jack@gmail.com", 65);
+    expect(testObject.getRole()).toBe("Manager");
+})
